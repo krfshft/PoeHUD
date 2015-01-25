ee6d0d89275a13e15c57e61752eee0d1ce51b806
crocodarrel <darrel343@gmail.com>
2015-01-25 13:27:11 -0800

    Added one decimal place in flat lifebar display for units above 1k life.

--- src/Framework/Helpers/ConvertHelper.cs
+++ src/Framework/Helpers/ConvertHelper.cs
@@ -1,14 +1,23 @@
-﻿namespace PoeHUD.Framework.Helpers
+﻿using System;
+
+namespace PoeHUD.Framework.Helpers
 {
-    public static class ConvertHelper
-    {
-        public static string ToShorten(double value, string format = "0")
-        {
-            if (value >= 1000000)
-            {
-                return string.Concat((value / 1000000).ToString(format), "M");
-            }
-            return value >= 1000 ? string.Concat((value / 1000).ToString(format), "K") : value.ToString(format);
-        }
-    }
+	public static class ConvertHelper
+	{
+		public static string ToShorten(double value, string format = "0")
+		{
+			if (value >= 1000000)
+			{
+				return string.Concat((value / 1000000).ToString(format), "M");
+			}
+
+			if (value >= 1000)
+			{
+				value = Math.Round((float)value / (float)1000, 2);
+				return string.Concat(value.ToString("F1"), "K");
+			}
+
+			return value.ToString(format);
+		}
+	}
 }
